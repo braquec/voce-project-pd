da1d5c2e91e222eb277f5227462a50f56831e589
braquec <sr.berch@gmail.com>
2025-12-05 13:11:17 -0600

    actualiza api

--- api/main.py
+++ api/main.py
@@ -48,30 +48,28 @@ def predict_topic(data: TextIn):
     """
     Recibe un comentario de texto libre y asigna el tópico principal.
     """
-    
-    # 1. Limpieza de Texto (misma función usada en el entrenamiento)
+
     cleaned_text = clean_text(data.comment)
     
-    # 2. Vectorización (usando el vectorizador entrenado)
+    # vectorización (usando el vectorizador entrenado)
     vectorized_text = vectorizer.transform([cleaned_text])
     
-    # 3. Predicción (obtener la distribución de probabilidades por tópico)
+    # predicción
     topic_distribution = lda_model.transform(vectorized_text)[0]
     
-    # 4. Asignación del Tópico Principal (el que tenga mayor probabilidad)
+    # asignacion topico principal
     topic_id = int(topic_distribution.argmax())
     
-    # 5. Lógica de Riesgo (simplificada)
-    # Por ejemplo, cualquier tópico con ID 10, 11 o 2 es de alto riesgo
+    # clasificamos topicos 10, 11 o 2 como alto riesgo
     is_hot = topic_id in [10, 11, 2] 
     
-    # 6. Devolver Resultado
+    # devolver Resultado
     return {
         "topic_id": topic_id,
         "topic_name": TOPIC_NAMES.get(topic_id, f"Tópico {topic_id} - No Nombrado"),
         "is_hot": is_hot
     }
 
-# Para ejecutar la API localmente (opcional)
+# ejecucion local
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
